c81dccea0737f46b0db6b773997bb822bd0f5210
Kowshik Kuri <ratulkuri@gmail.com>
2023-07-15 19:18:52 +0600

    Add minimum height to accordion Item

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -4,7 +4,7 @@ const AccordionItem = ({className, serial, title, content, isOpen, toggle}) => {
   return (
     <>
         <div className={`${isOpen ? "open" : ""} ${className?.length > 0 ? className : ""} acordion-item rounded-lg shadow-lg mb-4 overflow-hidden`}>
-            <div className="accordion-header cursor-pointer relative flex items-center gap-4 py-1.5 sm:py-4 pl-16 sm:pl-20 pr-10 sm:pr-16 bg-gradient-to-r from-cyan-500 to-blue-500" onClick={toggle}>
+            <div className="accordion-header min-h-[60px] cursor-pointer relative flex items-center gap-4 py-1.5 sm:py-4 pl-16 sm:pl-20 pr-10 sm:pr-16 bg-gradient-to-r from-cyan-500 to-blue-500" onClick={toggle}>
                 <span className="serial absolute font-bold left-0 -top-2 sm:-top-4 text-white text-5xl sm:text-6xl opacity-20">{serial > 9 ? serial : `0${serial}`}</span>
                 <h4 className="title text-white font-medium">{title}</h4>
                 <div className="collapse-icon text-white absolute right-3 sm:right-6">
