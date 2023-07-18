fb5fbcf8277e68268b3c481e82e9ec0df2a958fa
Kowshik Kuri <ratulkuri@gmail.com>
2023-07-19 00:19:03 +0600

    Improvement in bank open card

--- src/pages/Bank/Partials/AccountOpenCard.jsx
+++ src/pages/Bank/Partials/AccountOpenCard.jsx
@@ -16,18 +16,19 @@ const AccountOpenCard = ({name = "", setName, openAccount, min = 500}) => {
     return (
         <>
             <div className="w-full max-w-md p-8 border border-gray-300 shadow-lg rounded-lg focus-within:shadow-sm bg-gradient-to-r from-cyan-500 to-blue-500">
-                <h2 className="text-center text-white text-3xl font-black uppercase mb-10">Ethereal BANK</h2>
+                <h2 className="text-center text-white text-3xl font-black uppercase mb-2">Ethereal BANK</h2>
+                <h6 className="text-center text-white text-md font-medium uppercase mb-10">open an account to start banking</h6>
                 <form onSubmit={handleOpen}>
                     <input
                         type="text"
                         value={name}
                         onChange={(e) => setName(e.target.value)}
                         placeholder="Your Name"
-                        className="text-center text-xl text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 focus:outline-indigo-600 shadow-sm placeholder-gray-400 focus:ring-0 w-full mb-6"
+                        className="text-center text-xl text-gray-800 bg-white border rounded leading-5 py-2 px-3 border-gray-200 hover:border-gray-300 focus:border-indigo-300 focus:outline-indigo-600 shadow-sm placeholder-gray-400 focus:ring-0 w-full mb-6 placeholder:text-lg"
                     />
                     <InputAmount
-                        placeholder="Opening amount minimum 1000"
-                        inputClassName="text-center text-xl focus:outline-indigo-600"
+                        placeholder="Minimum amount 1000 BDT"
+                        inputClassName="text-center text-xl focus:outline-indigo-600 placeholder:text-lg"
                         className="mb-6"
                         value={openingAmmount}
                         set={setOpeningAmmount}
